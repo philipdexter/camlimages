f2cb9e417c2f6da3a7ea02f9a9375dfee270c391
Jun Furuse <jun.furuse@gmail.com>
2013-05-19 20:40:18 +0800

    C interface fix

--- src/jpegread.c
+++ src/jpegread.c
@@ -45,6 +45,12 @@
 
 #include <setjmp.h>
 
+#ifdef DEBUG_JPEG
+#  define DEBUGF(...) do{fprintf(stderr, __VA_ARGS__); fflush(stderr); }while(0)
+#else
+#  define DEBUGF(...) do{/*do nothing*/}while(0)
+#endif
+
 char jpg_error_message[JMSG_LENGTH_MAX];
  
 struct my_error_mgr {
@@ -56,182 +62,18 @@ struct my_error_mgr {
 typedef struct my_error_mgr * my_error_ptr;
 
 void
-my_error_exit (j_common_ptr cinfo)
+my_error_exit (j_common_ptr cinfop)
 {
   /* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
-  my_error_ptr myerr = (my_error_ptr) cinfo->err;
+  my_error_ptr myerr = (my_error_ptr) cinfop->err;
 
   /* Always display the message. */
   /* We could postpone this until after returning, if we chose. */
-  fprintf(stderr,"setting message\n");
-  (*cinfo->err->format_message) (cinfo, jpg_error_message );
+  fprintf(stderr,"setting libjpeg error message: \"%s\"\n", jpg_error_message);
+  (*cinfop->err->format_message) (cinfop, jpg_error_message );
 
   /* Return control to the setjmp point */
   longjmp(myerr->setjmp_buffer, 1);
-}
-
-GLOBAL(value)
-read_JPEG_file (value name)
-{
-  CAMLparam1(name);
-  CAMLlocal1(res);
-
-  char *filename;
-  /* This struct contains the JPEG decompression parameters and pointers to
-   * working space (which is allocated as needed by the JPEG library).
-   */
-  struct jpeg_decompress_struct cinfo;
-  /* We use our private extension JPEG error handler.
-   * Note that this struct must live as long as the main JPEG parameter
-   * struct, to avoid dangling-pointer problems.
-   */
-  struct my_error_mgr jerr;
-  /* More stuff */
-  FILE * infile;		/* source file */
-  JSAMPARRAY buffer;		/* Output row buffer */
-  int row_stride;		/* physical row width in output buffer */
-  int i;
-
-  filename= String_val( name );
-
-  /* In this example we want to open the input file before doing anything else,
-   * so that the setjmp() error recovery below can assume the file is open.
-   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
-   * requires it in order to read binary files.
-   */
-
-  if ((infile = fopen(filename, "rb")) == NULL) {
-    failwith("failed to open jpeg file");
-  }
-
-  /* Step 1: allocate and initialize JPEG decompression object */
-
-  /* We set up the normal JPEG error routines, then override error_exit. */
-  cinfo.err = jpeg_std_error(&jerr.pub);
-  jerr.pub.error_exit = my_error_exit;
-
-  /* Establish the setjmp return context for my_error_exit to use. */
-  if (setjmp(jerr.setjmp_buffer)) {
-    /* If we get here, the JPEG code has signaled an error.
-     * We need to clean up the JPEG object, close the input file, and return.
-     */
-    fprintf(stderr, "Exiting...");
-    jpeg_destroy_decompress(&cinfo);
-    fclose(infile);
-    exit(-1);
-    failwith(jpg_error_message);
-  }
-  /* Now we can initialize the JPEG decompression object. */
-  jpeg_create_decompress(&cinfo);
-
-  /* Step 2: specify data source (eg, a file) */
-
-  jpeg_stdio_src(&cinfo, infile);
-
-  /* Step 3: read file parameters with jpeg_read_header() */
-
-  (void) jpeg_read_header(&cinfo, TRUE);
-
-  /* We can ignore the return value from jpeg_read_header since
-   *   (a) suspension is not possible with the stdio data source, and
-   *   (b) we passed TRUE to reject a tables-only JPEG file as an error.
-   * See libjpeg.doc for more info.
-   */
-
-  /* Step 4: set parameters for decompression */
-
-  /* In this example, we don't need to change any of the defaults set by
-   * jpeg_read_header(), so we do nothing here.
-   */
-
-  cinfo.out_color_space = JCS_RGB;
-
-  /* Step 5: Start decompressor */
-
-  (void) jpeg_start_decompress(&cinfo);
-  /* We can ignore the return value since suspension is not possible
-   * with the stdio data source.
-   */
-
-  /* We may need to do some setup of our own at this point before reading
-   * the data.  After jpeg_start_decompress() we have the correct scaled
-   * output image dimensions available, as well as the output colormap
-   * if we asked for color quantization.
-   * In this example, we need to make an output work buffer of the right size.
-   */ 
-  /* JSAMPLEs per row in output buffer */
-
-  if( oversized(cinfo.output_width, cinfo.output_components) ){
-    jpeg_destroy_decompress(&cinfo);
-    fclose(infile);
-    failwith_oversized("jpeg");
-  }
-
-  row_stride = cinfo.output_width * cinfo.output_components;
-
-  /* Make a one-row-high sample array that will go away when done with image */
-  buffer = (*cinfo.mem->alloc_sarray)
-		((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 
-		 cinfo.output_height );
-
-  /* Step 6: while (scan lines remain to be read) */
-  /*           jpeg_read_scanlines(...); */
-
-  /* Here we use the library's state variable cinfo.output_scanline as the
-   * loop counter, so that we don't have to keep track ourselves.
-   */
-  while (cinfo.output_scanline < cinfo.output_height) {
-    /* jpeg_read_scanlines expects an array of pointers to scanlines.
-     * Here the array is only one element long, but you could ask for
-     * more than one scanline at a time if that's more convenient.
-     */
-    jpeg_read_scanlines(&cinfo, buffer + cinfo.output_scanline, 1); 
-  }
-
-  if( oversized(row_stride, cinfo.output_height) ){
-    jpeg_destroy_decompress(&cinfo);
-    fclose(infile);
-    failwith_oversized("jpeg");
-  }
-
-  {
-    CAMLlocalN(r,3);
-    r[0] = Val_int(cinfo.output_width);
-    r[1] = Val_int(cinfo.output_height);
-    r[2] = alloc_string ( row_stride * cinfo.output_height );
-    for(i=0; i<cinfo.output_height; i++){
-      memcpy( String_val(r[2]) + i * row_stride, 
-	       buffer[i], row_stride);
-    }
-    res = alloc_tuple(3);
-    for(i=0; i<3; i++) Field(res, i) = r[i];
-  }
-
-  /* Step 7: Finish decompression */
-
-  (void) jpeg_finish_decompress(&cinfo);
-  /* We can ignore the return value since suspension is not possible
-   * with the stdio data source.
-   */
-
-  /* Step 8: Release JPEG decompression object */
-
-  /* This is an important step since it will release a good deal of memory. */
-  jpeg_destroy_decompress(&cinfo);
-
-  /* After finish_decompress, we can close the input file.
-   * Here we postpone it until after no more JPEG errors are possible,
-   * so as to simplify the setjmp error logic above.  (Actually, I don't
-   * think that jpeg_destroy can do an error exit, but why assume anything...)
-   */
-  fclose(infile);
-
-  /* At this point you may want to check to see whether any corrupt-data
-   * warnings occurred (test whether jerr.pub.num_warnings is nonzero).
-   */
-
-  /* And we're done! */
-  CAMLreturn(res);
 }
 
 value jpeg_set_scale_denom( jpegh, denom )
@@ -247,11 +89,48 @@ value jpeg_set_scale_denom( jpegh, denom )
   CAMLreturn(Val_unit);
 }
 
+value caml_val_jpeg_marker( jpeg_saved_marker_ptr p )
+{
+    CAMLparam0();
+    CAMLlocal2(res, tmp);
+
+    tmp = caml_alloc_string(p->data_length);
+    memcpy( String_val(tmp), p->data, p->data_length);
+
+    res = alloc_small(2,0);
+    Field(res, 0) = Val_int(p->marker);
+    Field(res, 1) = tmp;
+    // fprintf(stderr, "mark %x %d\n", p->marker, p->data_length);
+
+    CAMLreturn(res);
+}
+
+value caml_val_jpeg_rev_markers( jpeg_saved_marker_ptr q )
+{
+    CAMLparam0();
+    CAMLlocal3(res, tmp, hd);
+    jpeg_saved_marker_ptr p = q;
+    res = Val_int(0); // null
+    while(p){
+        hd = caml_val_jpeg_marker(p);
+
+        // This cannot come before the call of caml_val_jpeg_marker!
+        // New allocation with non-initialized blocks crashes GC!
+        tmp = alloc_small(2,0); 
+        Field(tmp, 0) = hd;
+        Field(tmp, 1) = res;
+        res = tmp;
+        p = p->next;
+    }
+    CAMLreturn(res);
+}
+
 value open_jpeg_file_for_read( name )
      value name;
 {
   CAMLparam1(name);
   CAMLlocal1(res);
+  CAMLlocalN(r,4);
 
   char *filename;
   /* This struct contains the JPEG decompression parameters and pointers to
@@ -305,21 +184,28 @@ value open_jpeg_file_for_read( name )
 
   jpeg_stdio_src(cinfop, infile);
 
+  /* EXIF and other markers */
+
+  jpeg_save_markers(cinfop, JPEG_COM, 0xFFFF /* all */);
+  for(i=0; i<16; i++)
+      jpeg_save_markers(cinfop, JPEG_APP0+i, 0xFFFF /* all */);
+
   /* Step 3: read file parameters with jpeg_read_header() */
 
   (void) jpeg_read_header(cinfop, TRUE);
 
-  { 
-    CAMLlocalN(r,3);
-    r[0] = Val_int(cinfop->image_width);
-    r[1] = Val_int(cinfop->image_height);
-    r[2] = alloc_tuple(3);
-    Field(r[2], 0) = (value)cinfop;
-    Field(r[2], 1) = (value)infile;
-    Field(r[2], 2) = (value)jerrp;
-    res = alloc_tuple(3);
-    for(i=0; i<3; i++) Field(res, i) = r[i];
-  }
+  r[0] = Val_int(cinfop->image_width);
+  r[1] = Val_int(cinfop->image_height);
+  r[2] = alloc_small(3,0);
+  Field(r[2], 0) = (value)cinfop;
+  Field(r[2], 1) = (value)infile;
+  Field(r[2], 2) = (value)jerrp;
+
+  r[3] = caml_val_jpeg_rev_markers( cinfop->marker_list );
+
+  res = alloc_small(4,0);
+  for(i=0; i<4; i++) Field(res, i) = r[i];
+
   CAMLreturn(res);
 }
 
@@ -328,6 +214,7 @@ value open_jpeg_file_for_read_start( jpegh )
 {
   CAMLparam1(jpegh);
   CAMLlocal1(res);
+  CAMLlocalN(r,3);
   struct jpeg_decompress_struct* cinfop;
   struct my_error_mgr *jerrp;
   FILE *infile;
@@ -354,6 +241,8 @@ value open_jpeg_file_for_read_start( jpegh )
   /* Step 5: Start decompressor */
 
   (void) jpeg_start_decompress(cinfop);
+  DEBUGF("jpeg_start_decompress");
+
   /* We can ignore the return value since suspension is not possible
    * with the stdio data source.
    */
@@ -368,22 +257,17 @@ value open_jpeg_file_for_read_start( jpegh )
 
   /* row_stride = cinfop->output_width * cinfop->output_components; */
 
-  { 
-    CAMLlocalN(r,3);
-    // CR jfuruse: integer overflow
-    r[0] = Val_int(cinfop->output_width);
-    r[1] = Val_int(cinfop->output_height);
-    r[2] = alloc_tuple(3);
-    Field(r[2], 0) = (value)cinfop;
-    Field(r[2], 1) = (value)infile;
-    Field(r[2], 2) = (value)jerrp;
-    res = alloc_tuple(3);
-    for(i=0; i<3; i++) Field(res, i) = r[i];
-  }
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "cinfop= %d infile= %d %d %d \n", cinfop, infile, cinfop->output_scanline, cinfop->output_height); 
-  fflush(stderr);
-#endif
+  // CR jfuruse: integer overflow
+  r[0] = Val_int(cinfop->output_width);
+  r[1] = Val_int(cinfop->output_height);
+  r[2] = alloc_small(3,0);
+  Field(r[2], 0) = (value)cinfop;
+  Field(r[2], 1) = (value)infile;
+  Field(r[2], 2) = (value)jerrp;
+  res = alloc_small(3,0);
+  for(i=0; i<3; i++) Field(res, i) = r[i];
+
+  DEBUGF("cinfop= %d infile= %d %d %d \n", (int)cinfop, (int)infile, cinfop->output_scanline, cinfop->output_height); 
   CAMLreturn(res);
 }
 
@@ -429,38 +313,26 @@ value close_jpeg_file_for_read( jpegh )
   struct my_error_mgr *jerrp;
   FILE *infile;
 
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "closing\n");
-  fflush(stderr);
-#endif
+  DEBUGF("closing\n");
 
   cinfop = (struct jpeg_decompress_struct *) Field( jpegh, 0 );
   infile = (FILE *) Field( jpegh, 1 );
   jerrp = (struct my_error_mgr *) Field( jpegh, 2 );
 
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "cinfop= %d infile= %d %d %d \n", cinfop, infile, cinfop->output_scanline, cinfop->output_height); 
-#endif
+  DEBUGF("cinfop= %d infile= %d %d %d \n", (int)cinfop, (int)infile, cinfop->output_scanline, cinfop->output_height); 
 
-  if( cinfop->output_scanline >= cinfop->output_height ){ 
-#ifdef DEBUG_JPEG
-    fprintf(stderr, "finish\n");
-    fflush(stderr);
-#endif
-    jpeg_finish_decompress( cinfop );
+  if( cinfop->output_height > 0 ){ // == 0 means jpeg_start_decompress is never called
+      if( cinfop->output_scanline >= cinfop->output_height ){ 
+          DEBUGF("finish_decompress\n");
+          jpeg_finish_decompress( cinfop );
+      }
   }
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "destroy\n");
-  fflush(stderr);
-#endif
+  DEBUGF("destroy\n");
   jpeg_destroy_decompress( cinfop ); 
   
   free(cinfop);
   free(jerrp);
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "file close\n");
-  fflush(stderr);
-#endif
+  DEBUGF("file close\n");
   fclose(infile);
 
   CAMLreturn(Val_unit);
--- src/jpegwrite.c
+++ src/jpegwrite.c
@@ -38,6 +38,12 @@
  * and a compression quality factor are passed in.
  */
 
+#ifdef DEBUG_JPEG
+#  define DEBUGF(...) do{fprintf(stderr, __VA_ARGS__); fflush(stderr); }while(0)
+#else
+#  define DEBUGF(...) do{/*do nothing*/}while(0)
+#endif
+
 extern char jpg_error_message[JMSG_LENGTH_MAX];
 struct my_error_mgr {
   struct jpeg_error_mgr pub;	/* "public" fields */
@@ -47,159 +53,6 @@ struct my_error_mgr {
 typedef struct my_error_mgr * my_error_ptr;
 extern void my_error_exit (j_common_ptr);
 
-GLOBAL(value)
-write_JPEG_file (value file,
-                 value buffer,
-		 value width, 
-		 value height, 
-		 value qual)
-{
-  JSAMPLE *image_buffer;
-  int image_height;
-  int image_width;
-  char *filename;
-  int quality;
-
-  /* This struct contains the JPEG compression parameters and pointers to
-   * working space (which is allocated as needed by the JPEG library).
-   * It is possible to have several such structures, representing multiple
-   * compression/decompression processes, in existence at once.  We refer
-   * to any one struct (and its associated working data) as a "JPEG object".
-   */
-  struct jpeg_compress_struct cinfo;
-  /* This struct represents a JPEG error handler.  It is declared separately
-   * because applications often want to supply a specialized error handler
-   * (see the second half of this file for an example).  But here we just
-   * take the easy way out and use the standard error handler, which will
-   * print a message on stderr and call exit() if compression fails.
-   * Note that this struct must live as long as the main JPEG parameter
-   * struct, to avoid dangling-pointer problems.
-   */
-  struct jpeg_error_mgr jerr;
-  /* More stuff */
-  FILE * outfile;		/* target file */
-  JSAMPROW row_pointer[1];	/* pointer to JSAMPLE row[s] */
-  int row_stride;		/* physical row width in image buffer */
-
-  image_buffer = String_val(buffer);
-  image_width = Int_val(width);
-  image_height = Int_val(height);
-  filename = String_val(file);
-  quality= Int_val(qual);
-
-  /* Step 1: allocate and initialize JPEG compression object */
-
-  /* We have to set up the error handler first, in case the initialization
-   * step fails.  (Unlikely, but it could happen if you are out of memory.)
-   * This routine fills in the contents of struct jerr, and returns jerr's
-   * address which we place into the link field in cinfo.
-   */
-  cinfo.err = jpeg_std_error(&jerr);
-  /* Now we can initialize the JPEG compression object. */
-  jpeg_create_compress(&cinfo);
-
-  /* Step 2: specify data destination (eg, a file) */
-  /* Note: steps 2 and 3 can be done in either order. */
-
-  /* Here we use the library-supplied code to send compressed data to a
-   * stdio stream.  You can also write your own code to do something else.
-   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
-   * requires it in order to write binary files.
-   */
-  if ((outfile = fopen(filename, "wb")) == NULL) {
-    failwith ("can't open %s\n");
-  }
-  jpeg_stdio_dest(&cinfo, outfile);
-
-  /* Step 3: set parameters for compression */
-
-  /* First we supply a description of the input image.
-   * Four fields of the cinfo struct must be filled in:
-   */
-  cinfo.image_width = image_width; 	/* image width and height, in pixels */
-  cinfo.image_height = image_height;
-  cinfo.input_components = 3;		/* # of color components per pixel */
-  cinfo.in_color_space = JCS_RGB; 	/* colorspace of input image */
-  /* Now use the library's routine to set default compression parameters.
-   * (You must set at least cinfo.in_color_space before calling this,
-   * since the defaults depend on the source color space.)
-   */
-  jpeg_set_defaults(&cinfo);
-  /* Now you can set any non-default parameters you wish to.
-   * Here we just illustrate the use of quality (quantization table) scaling:
-   */
-  jpeg_set_quality(&cinfo, quality, TRUE /* limit to baseline-JPEG values */);
-
-  /* Step 4: Start compressor */
-
-  /* TRUE ensures that we will write a complete interchange-JPEG file.
-   * Pass TRUE unless you are very sure of what you're doing.
-   */
-  jpeg_start_compress(&cinfo, TRUE);
-
-  /* Step 5: while (scan lines remain to be written) */
-  /*           jpeg_write_scanlines(...); */
-
-  /* Here we use the library's state variable cinfo.next_scanline as the
-   * loop counter, so that we don't have to keep track ourselves.
-   * To keep things simple, we pass one scanline per call; you can pass
-   * more if you wish, though.
-   */
-  row_stride = image_width * 3;	/* JSAMPLEs per row in image_buffer */
-
-  while (cinfo.next_scanline < cinfo.image_height) {
-    /* jpeg_write_scanlines expects an array of pointers to scanlines.
-     * Here the array is only one element long, but you could pass
-     * more than one scanline at a time if that's more convenient.
-     */
-    row_pointer[0] = & image_buffer[cinfo.next_scanline * row_stride];
-    (void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
-  }
-
-  /* Step 6: Finish compression */
-
-  jpeg_finish_compress(&cinfo);
-  /* After finish_compress, we can close the output file. */
-  fclose(outfile);
-
-  /* Step 7: release JPEG compression object */
-
-  /* This is an important step since it will release a good deal of memory. */
-  jpeg_destroy_compress(&cinfo);
-
-  /* And we're done! */
-  return Val_unit;
-}
-
-
-/*
- * SOME FINE POINTS:
- *
- * In the above loop, we ignored the return value of jpeg_write_scanlines,
- * which is the number of scanlines actually written.  We could get away
- * with this because we were only relying on the value of cinfo.next_scanline,
- * which will be incremented correctly.  If you maintain additional loop
- * variables then you should be careful to increment them properly.
- * Actually, for output to a stdio stream you needn't worry, because
- * then jpeg_write_scanlines will write all the lines passed (or else exit
- * with a fatal error).  Partial writes can only occur if you use a data
- * destination module that can demand suspension of the compressor.
- * (If you don't know what that's for, you don't need it.)
- *
- * If the compressor requires full-image buffers (for entropy-coding
- * optimization or a multi-scan JPEG file), it will create temporary
- * files for anything that doesn't fit within the maximum-memory setting.
- * (Note that temp files are NOT needed if you use the default parameters.)
- * On some systems you may need to set up a signal handler to ensure that
- * temporary files are deleted if the program is interrupted.  See libjpeg.doc.
- *
- * Scanlines MUST be supplied in top-to-bottom order if you want your JPEG
- * files to be compatible with everyone else's.  If you cannot readily read
- * your data in that order, you'll need an intermediate array to hold the
- * image.  See rdtarga.c or rdbmp.c for examples of handling bottom-to-top
- * source data using the JPEG code's internal virtual-array mechanisms.
- */
-
 value open_jpeg_file_for_write_colorspace( name, width, height, qual, colorspace )
      value name;
      value width;
@@ -207,6 +60,8 @@ value open_jpeg_file_for_write_colorspace( name, width, height, qual, colorspace
      value qual;
      J_COLOR_SPACE colorspace;
 {
+  CAMLparam0();
+  CAMLlocal1(res);
   char *filename;
   int image_height;
   int image_width;
@@ -216,7 +71,6 @@ value open_jpeg_file_for_write_colorspace( name, width, height, qual, colorspace
   struct my_error_mgr *jerrp;
   /* More stuff */
   FILE * outfile;		/* source file */
-  value res;
 
   image_width= Int_val( width );
   image_height= Int_val( height );
@@ -253,17 +107,16 @@ value open_jpeg_file_for_write_colorspace( name, width, height, qual, colorspace
   jpeg_set_quality(cinfop, quality, TRUE);
   jpeg_start_compress(cinfop, TRUE);
 
-  { 
-    res = alloc_tuple(3);
-    Field(res, 0) = (value)cinfop;
-    Field(res, 1) = (value)outfile;
-    Field(res, 2) = (value)jerrp;
-  }
+  res = alloc_small(3,0);
+  Field(res, 0) = (value)cinfop;
+  Field(res, 1) = (value)outfile;
+  Field(res, 2) = (value)jerrp;
+
   /*
   fprintf(stderr, "cinfop= %d outfile= %d %d %d \n", cinfop, infile, cinfop->output_scanline, cinfop->output_height); 
   fflush(stderr);
   */
-  return res;
+  CAMLreturn(res);
 }
 
 value open_jpeg_file_for_write( name, width, height, qual )
@@ -286,6 +139,31 @@ value open_jpeg_file_for_write_cmyk( name, width, height, qual )
     open_jpeg_file_for_write_colorspace( name, width, height, qual, JCS_CMYK );
 }
 
+void caml_jpeg_write_marker( value jpegh, value raw )
+{
+    struct jpeg_compress_struct *cinfop;
+    cinfop = (struct jpeg_compress_struct *) Field( jpegh, 0 );
+
+    // EXTERN(void) jpeg_write_marker
+	// JPP((j_compress_ptr cinfo, int marker,
+    // const JOCTET * dataptr, unsigned int datalen));
+    
+    int code = Int_val(Field(raw,0));
+    char *data = String_val(Field(raw,1));
+    unsigned int len = caml_string_length(Field(raw,1));
+    jpeg_write_marker(cinfop, code, data, len); // This actually writes bytes, so data seems to be ok being GCed.
+}
+
+// You can write special markers immediately following the datastream header by
+// calling jpeg_write_marker() after jpeg_start_compress() and before the first
+// call to jpeg_write_scanlines().  When you do this, the markers appear after
+// the SOI and the JFIF APP0 and Adobe APP14 markers (if written), but before
+// all else.  Specify the marker type parameter as "JPEG_COM" for COM or
+// "JPEG_APP0 + n" for APPn.  (Actually, jpeg_write_marker will let you write
+// any marker type, but we don't recommend writing any other kinds of marker.)
+// For example, to write a user comment string pointed to by comment_text:
+// 	jpeg_write_marker(cinfo, JPEG_COM, comment_text, strlen(comment_text));
+
 value write_jpeg_scanline( jpegh, buf )
 value jpegh, buf;
 {
@@ -307,38 +185,24 @@ value close_jpeg_file_for_write( jpegh )
   struct my_error_mgr *jerrp;
   FILE *outfile;
 
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "closing\n");
-  fflush(stderr);
-#endif
+  DEBUGF( "closing\n");
 
   cinfop = (struct jpeg_compress_struct *) Field( jpegh, 0 );
   outfile = (FILE *) Field( jpegh, 1 );
   jerrp = (struct my_error_mgr *) Field( jpegh, 2 );
 
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "cinfop= %d outfile= %d %d %d \n", cinfop, outfile, cinfop->next_scanline, cinfop->image_height); 
-#endif
+  DEBUGF( "cinfop= %d outfile= %d %d %d \n", cinfop, outfile, cinfop->next_scanline, cinfop->image_height); 
 
    if( cinfop->next_scanline >= cinfop->image_height ){ 
-#ifdef DEBUG_JPEG
-     fprintf(stderr, "finish\n");
-     fflush(stderr);
-#endif
+     DEBUGF( "finish\n");
      jpeg_finish_compress( cinfop );
    }
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "destroy\n");
-  fflush(stderr);
-#endif
+  DEBUGF( "destroy\n");
   jpeg_destroy_compress( cinfop ); 
   
   free(cinfop);
   free(jerrp);
-#ifdef DEBUG_JPEG
-  fprintf(stderr, "file close\n");
-  fflush(stderr);
-#endif
+  DEBUGF( "file close\n");
   fclose(outfile);
 
   return Val_unit;
