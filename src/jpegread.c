4c7245986c8bc0abd2499d47aca9ddbd50f30466
Jun Furuse <Jun.Furuse@gmail.com>
2014-08-01 00:40:11 +0800

    Fixed C compilation issue (CAMLreturn0 must not return value)

--- src/jpegread.c
+++ src/jpegread.c
@@ -286,7 +286,7 @@ value jpegh, offset, buf;
 }
 
 /* no boundary checks */
-value read_jpeg_scanlines( value jpegh, value buf, value offset, value lines )
+void read_jpeg_scanlines( value jpegh, value buf, value offset, value lines )
 {
   CAMLparam4(jpegh,offset,buf,lines);
   struct jpeg_decompress_struct *cinfop;
